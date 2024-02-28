574f133272700437654c6159a7270a4c627dab1b
AraaCuteUwU <71029264+AraaCuteUwU@users.noreply.github.com>
2024-02-28 11:52:42 +0800

    Fix fortune & silktouch

--- src/main/java/me/kyaafira/autopickup/Main.java
+++ src/main/java/me/kyaafira/autopickup/Main.java
@@ -5,14 +5,16 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Collection;
 import java.util.Objects;
 
-public final class Main extends JavaPlugin implements Listener {
+public class Main extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
@@ -25,7 +27,8 @@ public final class Main extends JavaPlugin implements Listener {
     public void onBreak(@NotNull BlockBreakEvent event) {
         Player player = event.getPlayer();
         Block block = event.getBlock();
-        @NotNull Collection<ItemStack> drops = block.getDrops();
+        PlayerInventory inventory = player.getInventory();
+        @NotNull Collection<ItemStack> drops = block.getDrops(inventory.getItemInMainHand());
         boolean isFull = false;
         ItemStack[] itemsToAdd = new ItemStack[drops.toArray().length];
         int itemCount = 0;
@@ -36,25 +39,25 @@ public final class Main extends JavaPlugin implements Listener {
             if (player.getInventory().firstEmpty() == -1) {
                 isFull = true;
             }
+
             itemsToAdd[itemCount] = drop;
             itemCount++;
         }
 
         if (itemCount > 0) {
             for (int i = 0; i < itemCount; i++) {
-                player.getInventory().addItem(itemsToAdd[i]);
-                player.giveExp(event.getExpToDrop());
-                event.setExpToDrop(0);
+                if (!isFull) {
+                    player.getInventory().addItem(itemsToAdd[i]);
+                    player.giveExp(event.getExpToDrop());
+                } else {
+                    player.sendMessage(Objects.requireNonNull(Objects.requireNonNull(this.getConfig().getString("full-inventory")).replace("&", "§")));
+                    event.setCancelled(true);
+                }
             }
         }
 
         event.setDropItems(false);
-
-        if (isFull) {
-            player.sendMessage(Objects.requireNonNull(Objects.requireNonNull(this.getConfig().getString("full-inventory")).replace("&", "§")));
-            event.setCancelled(true);
-        }
-
+        event.setExpToDrop(0);
     }
 
     public boolean isWorldBlacklisted(Player player) {
